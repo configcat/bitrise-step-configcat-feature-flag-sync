e30e368a2d6eecab4bad82dc815fe8afee249d08
kp-cat <52385411+kp-cat@users.noreply.github.com>
2022-04-21 21:26:34 +0200

    docker

--- step.sh
+++ step.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -ex
 
+# docker pull configcat/cli
+# docker run --rm \
+#     --env CONFIGCAT_API_HOST=${api_host} \
+#     --env CONFIGCAT_API_USER=${api_user} \
+#     --env CONFIGCAT_API_PASS=${api_pass} \
+#     -v ${BITRISE_SOURCE_DIR}:/repository \
+#     configcat/cli scan "/repository/${sub_folder}" \
+#     --config-id ${config_id} \
+#     --repo=${GIT_REPOSITORY_URL} \
+#     --line-count=${line_count} \
+#     --file-url-template="$GIT_REPOSITORY_URL/blob/{commitHash}/{filePath}#L{lineNumber}" \
+#     --commit-url-template="$GIT_REPOSITORY_URL/commit/{commitHash}" \
+#     --runner="Bitrise Step Scan Repository" \
+#     --upload \
+#     --verbose=${verbose} \
+#     --non-interactive
+
 curl -fsSL "https://raw.githubusercontent.com/configcat/cli/main/scripts/install.sh" | bash -s -- -v=1.4.1
 
 export CONFIGCAT_API_HOST=${api_host}

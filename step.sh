2a0967ec470124868116e54e2f70f5fab958a051
kp-cat <52385411+kp-cat@users.noreply.github.com>
2022-04-27 11:30:45 +0200

    rename input variables

--- step.sh
+++ step.sh
@@ -3,12 +3,12 @@ set -e
 
 curl -fsSL "https://raw.githubusercontent.com/configcat/cli/main/scripts/install.sh" | bash -s -- -v=1.4.1
 
-export CONFIGCAT_API_HOST=${api_host}
-export CONFIGCAT_API_USER=${api_user}
-export CONFIGCAT_API_PASS=${api_pass}
+export CONFIGCAT_API_HOST=${configcat_api_host}
+export CONFIGCAT_API_USER=${configcat_api_user}
+export CONFIGCAT_API_PASS=${configcat_api_pass}
 
 configcat scan "$BITRISE_SOURCE_DIR/${sub_folder}" \
-    --config-id=${config_id} \
+    --config-id=${configcat_config_id} \
     --repo=${GIT_REPOSITORY_URL} \
     --line-count=${line_count} \
     --file-url-template="$GIT_REPOSITORY_URL/blob/{commitHash}/{filePath}#L{lineNumber}" \

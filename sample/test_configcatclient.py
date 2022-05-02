629fc9967a8cafd3b5b67a238670ec1307fc038a
kp-cat <52385411+kp-cat@users.noreply.github.com>
2022-05-02 21:45:20 +0200

    sample to scan

---
+++ sample/test_configcatclient.py
@@ -0,0 +1,50 @@
+import unittest
+
+from configcatclient import ConfigCatClientException
+from configcatclient.configcatclient import ConfigCatClient
+from configcatclienttests.mocks import ConfigCacheMock
+
+
+class ConfigCatClientTests(unittest.TestCase):
+    AwesomeFeatureEnabled = "isAwesomeFeatureEnabled"
+
+    def test_without_api_key(self):
+        try:
+            ConfigCatClient(None)
+            self.fail('Expected ConfigCatClientException')
+        except ConfigCatClientException:
+            pass
+
+    def test_bool(self):
+        client = ConfigCatClient('test', 0, 0, None, 0, config_cache_class=ConfigCacheMock)
+        self.assertEqual(True, client.get_value('testBoolKey', False))
+        client.stop()
+
+    def test_string(self):
+        client = ConfigCatClient('test', 0, 0, None, 0, config_cache_class=ConfigCacheMock)
+        self.assertEqual('testValue', client.get_value('testStringKey', 'default'))
+        client.stop()
+
+    def test_int(self):
+        client = ConfigCatClient('test', 0, 0, None, 0, config_cache_class=ConfigCacheMock)
+        self.assertEqual(1, client.get_value('testIntKey', 0))
+        client.stop()
+
+    def test_double(self):
+        client = ConfigCatClient('test', 0, 0, None, 0, config_cache_class=ConfigCacheMock)
+        self.assertEqual(1.1, client.get_value('testDoubleKey', 0.0))
+        client.stop()
+
+    def test_unknown(self):
+        client = ConfigCatClient('test', 0, 0, None, 0, config_cache_class=ConfigCacheMock)
+        self.assertEqual('default', client.get_value(self.AwesomeFeatureEnabled, 'default'))
+        client.stop()
+
+    def test_invalidation(self):
+        client = ConfigCatClient('test', 0, 0, None, 0, config_cache_class=ConfigCacheMock)
+        self.assertEqual(True, client.get_value('testBoolKey', False))
+        client.stop()
+
+
+if __name__ == '__main__':
+    unittest.main()

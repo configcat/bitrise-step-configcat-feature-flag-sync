836a4f0f44149093946676e2e315c65f0592b021
kp-cat <52385411+kp-cat@users.noreply.github.com>
2022-05-05 16:24:47 +0200

    true, false value_options for verbose input + remove api_host and api_user inputs

--- step.sh
+++ step.sh
@@ -4,8 +4,6 @@ set -e
 curl -fsSL "https://raw.githubusercontent.com/configcat/cli/main/scripts/install.sh" | bash -s -- -v=1.4.1
 
 export CONFIGCAT_API_HOST=${configcat_api_host}
-export CONFIGCAT_API_USER=${configcat_api_user}
-export CONFIGCAT_API_PASS=${configcat_api_pass}
 
 configcat scan "$BITRISE_SOURCE_DIR/${sub_folder}" \
     --config-id=${configcat_config_id} \

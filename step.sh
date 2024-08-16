69275e5418203ea42c272f4ec83de11dfd105b72
kp-cat <52385411+kp-cat@users.noreply.github.com>
2024-08-16 15:34:47 +0200

    exclude_keys, alias_patterns, usage_patterns options

--- step.sh
+++ step.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 
-curl -fsSL "https://raw.githubusercontent.com/configcat/cli/main/scripts/install.sh" | bash -s -- -v=2.0.0
+curl -fsSL "https://raw.githubusercontent.com/configcat/cli/main/scripts/install.sh" | bash -s -- -v=2.3.2
 
 export CONFIGCAT_API_HOST=${configcat_api_host}
+export CONFIGCAT_ALIAS_PATTERNS=${alias_patterns}
+export CONFIGCAT_USAGE_PATTERNS=${usage_patterns}
+
+EXCLUDE_FLAG_KEYS="' '"
+[ ! -z "${exclude_keys}" ] && EXCLUDE_FLAG_KEYS=${exclude_keys}
 
 configcat scan "$BITRISE_SOURCE_DIR/${sub_folder}" \
     --config-id=${configcat_config_id} \
@@ -14,4 +19,5 @@ configcat scan "$BITRISE_SOURCE_DIR/${sub_folder}" \
     --runner="Bitrise Step Scan Repository" \
     --upload \
     --verbose=${verbose} \
-    --non-interactive
+    --non-interactive \
+    --exclude-flag-keys ${EXCLUDE_FLAG_KEYS}
